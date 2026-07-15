ea77dee9bb31ff87b912678923910c066345e6f1
Zamad Shakeel <zamadshakil@gmail.com>
2026-07-16 04:49:02 +0500

    Implement custom unified sitemap.xml generator endpoint and remove sitemap index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://hashbooks.site',
-  integrations: [sitemap()],
   adapter: vercel(),
   prefetch: true,
   build: {
